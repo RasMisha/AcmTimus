ebefc584c7feaacdf0d4efe962953f08b6d76edf
mmaslov <mmaslov@evelopers.com>
2020-05-07 22:42:27 +0300

    - init commit - task template

---
+++ src/T0000.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class T0000 {
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    static MyScanner sc;
+
+    static {
+        try {
+            sc = new MyScanner();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        doTask();
+        out.flush();
+    }
+
+    public static void doTask(){
+    }
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() throws FileNotFoundException {
+            br = System.getProperty("ONLINE_JUDGE") != null
+                ? new BufferedReader(new InputStreamReader(System.in))
+                : new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
