6231fa6ff4383abeddee1b9e0aa5f82173bd5234
mmaslov <mmaslov@evelopers.com>
2020-05-19 20:11:39 +0300

    1794 - Masterpieces of World Architecture

---
+++ src/T1794.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class T1794 {
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    static MyScanner sc;
+
+    static {
+        try {
+            sc = new MyScanner();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        doTask();
+        out.flush();
+    }
+
+    public static void doTask(){
+        int n = sc.nextInt();
+        int[] v = new int[n];
+        for (int i=0; i<n;i++) {
+            v[(n + i - (sc.nextInt()-1))%n]++;
+        }
+        int max = -1;
+        int result = -1;
+        for (int i=0; i<n; i++) {
+            if (v[i] > max) {
+                max = v[i];
+                result = (i+1);
+            }
+        }
+        out.println(result);
+    }
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() throws FileNotFoundException {
+            br = System.getProperty("ONLINE_JUDGE") != null
+                    ? new BufferedReader(new InputStreamReader(System.in))
+                    : new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
